d78d91b30d3be04247d5e299f043ceb3dbedfd6a
sd2001 <im.swarnabha2001@gmail.com>
2020-09-04 01:16:41 +0530

    Updating almost everything in app.py

--- app.py
+++ app.py
@@ -10,33 +10,50 @@ client=MongoClient("mongodb+srv://swarnabha:swarnabhadb@cluster0.v3eq0.mongodb.n
 app=Flask(__name__)
 db=client['Posts']
 db2=client['Users']
-
+det=[1,3]
 app.secret_key = 'hellouserapi'
 
 # contents=[{"title":"Flask",
 #            "content":"Micro Web framework used for deployment using python",
 #            "author":"Someone maybe :-)"}]
 
-@app.route('/')
+@app.route('/posts')
 def home():
-    details=db.details
-    return render_template('posts.html',contents=details)
+    if g.user:
+        details=db.details
+        return render_template('posts.html',contents=details)
+    return render_template('login.html')
+
+@app.route('/mypost')
+def mypost():
+    if g.user:
+        pp=db.details
+        mq={'author':g.user}
+        details=pp.find(mq)
+        return render_template('my_posts.html',contents=details)
 
 @app.route('/create')
-def create_p():
-    return render_template('create_blog.html')
+def create():
+    if g.user:        
+        return render_template('create_blog.html',name=g.user)
+    return render_template('login.html')
+
 
 @app.route('/create',methods=['POST'])
-def create():
-    title=request.form.get('title')
-    content=request.form.get('content')
-    author="SD"
-    doc={'title':title,
-         'content':content,
-         'author':author}
-    details=db.details
-    details.insert_one(doc)
-    return redirect(url_for('home'))
+def create_p():
+    if g.user:    
+        title=request.form.get('title')
+        content=request.form.get('content')
+        author=g.user
+        doc={'title':title,
+            'content':content,
+            'author':author}
+        details=db.details
+        details.insert_one(doc)
+        return redirect(url_for('home'))
+    
+    return render_template('login.html')
+
 
 @app.route('/login')
 def login():
@@ -61,9 +78,9 @@ def login_p():
                 flag=True
                 session['user']=name 
                 print(session['user'])  
-                det[0]=name
-                det[1]=email                                  
-                return redirect(url_for('/'))
+                # det[0]=name
+                # det[1]=email                                  
+                return redirect(url_for('home'))
         
     if flag==False:
         flash("Invalid Credentials")
@@ -96,14 +113,20 @@ def register_p():
     data.insert_one(user_info)
         
     return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return render_template('login.html')
+    
     
 
-
-@app.route('/mypost')
-def mypost():
-    details=db.details
-    return render_template('my_posts.html',contents=details)
-
+@app.before_request
+def before_request():
+    g.user=None
+    det=[]
+    if 'user' in session:
+        g.user=session['user']
     
 
 if __name__=='__main__':
